0f42acc03f91893f431f478ecfa92fafa46d335f
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-06-24 23:14:33 -0300

    feat/#11-footer: completo el footer

--- src/components/molecule/Footer.tsx
+++ src/components/molecule/Footer.tsx
@@ -1,11 +1,21 @@
-interface Props {
-  title: string
+import { Link } from 'react-router-dom'
+import { Typography } from '@mui/material'
+import { YEAR } from '../../utils/constants'
+import './Footer.style.css'
+
+interface FooterProps {
+  text: string
 }
 
-export const Footer: React.FC<Props> = ({ title }) => {
+export const Footer: React.FC<FooterProps> = ({ text }) => {
   return (
-    <main>
-      <h1>{ title }</h1>
-    </main>
+    <footer>
+      <Typography align="right" gutterBottom>
+        Designed by <Link to="https://www.linkedin.com/in/maríaeugeniacosta/" target="_blank">María Eugenia Costa</Link > and <Link to="https://github.com/flopixx" target="_blank">Florencia Velazquez</Link>
+      </Typography>
+      <Typography gutterBottom>
+        &copy; Copyright { YEAR } - { text }
+      </Typography>
+    </footer>
   )
 }
