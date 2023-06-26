a39e16f2520f2e07369798a96633c2a383a44c16
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-06-26 20:30:54 -0300

    creo el componente BackGroundSection que es un wrapper para tener adentro contenido y fondo en imagen

---
+++ src/components/atom/BackgroundSection.tsx
@@ -0,0 +1,23 @@
+import children from 'react'
+import { Box } from '@mui/material'
+
+/*
+ * Reusable component for a section with a background image:
+ * - srcImg: is the src of the image display as a background
+ */
+interface BackgroundSectionProps {
+  srcImg?: string
+  children?: React.ReactNode
+}
+
+export const BackgroundSection: React.FC<BackgroundSectionProps> = ({ srcImg, children }) => {
+  return (
+    <Box sx={ {
+      width: '100%',
+      height: '100vh',
+      backgroundImage: `url(${ srcImg })`
+    } }>
+      { children }
+    </Box>
+  )
+}
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,6 @@
-import { Headline } from "../components/atom/Headline"
+import { BackgroundSection } from '../components/atom/BackgroundSection'
+import { Headline } from '../components/atom/Headline'
+import backgroundImage from '../assets/bg.jpg'
 
 interface ContactProps {
   title: string
@@ -7,7 +9,9 @@ interface ContactProps {
 export const Contact: React.FC<ContactProps> = ({ title }) => {
   return (
     <main>
-      <Headline title={title}/>
+      <BackgroundSection srcImg={ backgroundImage }>
+        <Headline title={ title } />
+      </BackgroundSection>
     </main>
   )
 }
