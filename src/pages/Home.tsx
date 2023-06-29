b914fdd10276ed8bf04acd07cd7cb1a359d781d7
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-06-28 21:06:08 -0300

    feat/#18-login: hago el form de login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
 import { Headline } from '../components/atom/Headline'
-import { Loader } from '../components/atom/Loader'
-
 interface HomeProps {
   title: string
 }
@@ -9,7 +7,6 @@ export const Home: React.FC<HomeProps> = ({ title }) => {
   return (
     <main>
       <Headline title={ title } />
-      <Loader title="Loading..."/>
     </main>
   )
 }
