3a80c93b1321befdf6d83a95b56111794bf6a7dc
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-16 15:15:20 -0300

    #feat/#26-not-found-page -complete the not funf page with a background image, teh text and a button to home

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,13 +1,42 @@
-import { Headline } from "../components/atom/Headline"
+import { useNavigate } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { BackgroundSection } from '../components/atom/BackgroundSection'
+import { Headline } from '../components/atom/Headline'
+import PrimaryButton from '../components/atom/PrimaryButton'
+import backgroundImage from '../assets/bg.jpg'
 
 interface NotFoundProps {
-  title: string
 }
 
-export const NotFound: React.FC<NotFoundProps> = ({ title }) => {
+export const NotFound: React.FC<NotFoundProps> = () => {
+  const navigate = useNavigate()
+  const handleClick = () => navigate('/')
+
   return (
     <main>
-      <Headline title={title}/>
-    </main>
+      <BackgroundSection srcImg={ backgroundImage } srcHeight='90vh'>
+        <Box
+          sx={ {
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            flexWrap: 'wrap',
+            alignContent: 'center',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: '1rem'
+        }}
+        >
+        <Headline
+          title='Ups ... recipe not found'
+          colorHeadline='black'
+        />
+        <PrimaryButton
+          textBtn='BACK TO HOME'
+          onClick={ handleClick }
+        />
+      </Box>
+    </BackgroundSection>
+    </main >
   )
 }
