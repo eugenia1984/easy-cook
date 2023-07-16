d0acd13425330baf81297ce921bd6261a06687e3
eugenia1984 <mariaeugeniacosta1@gmail.com>
2023-07-16 14:27:00 -0300

    #feat/#24-cta-section -create all the section

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,3 +1,7 @@
 // Type for the buttons props
 export type sizeBtn = 'small' | 'medium' | 'large'
 export type colorBtn = 'inherit' | 'primary' | 'secondary' | 'success' | 'error' | 'info' | 'warning'
+
+// Type for the title
+export type variantHeadline = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'subtitle1' | 'subtitle2'
+export type alignHeadline = 'right' | 'left' | 'center' | 'justify'
